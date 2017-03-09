f3d981de816eed6a792f91cbb0aef29d0fb3751e
Tetiana Korchak <tkorchak@mirantis.com>
2017-03-09 14:37:48 -0800

    renamed script

--- deploy_rally.sh
+++ deploy_rally.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-yum install git
-yum install docker
+yum -y install git
+yum -y install docker
 
 git clone https://github.com/AlexBalenko/mcp-rally-verify.git
 cd mcp-rally-verify/
 sed -i -- 's/keystonerc/openrc/g' *
 ./prepare_env.sh
+git clone https://github.com/kotavi/docker_rally.git
+cd docker_rally/
+./fix_deployment_config.sh
