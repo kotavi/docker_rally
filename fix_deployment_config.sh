d25740866b1382fb0056cd1bf2e9255615dc8675
Tetiana Korchak <tkorchak@mirantis.com>
2017-03-09 15:11:34 -0800

    refactored code

--- fix_deployment_config.sh
+++ fix_deployment_config.sh
@@ -5,6 +5,10 @@ source /home/rally/openrc
 rally-manage db recreate
 rally deployment create --fromenv --name=tempest
 
+rally verify create-verifier --name tempest_tests --type tempest --source https://github.com/openstack/tempest.git
+rally verify list-verifiers
+rally verify show-verifier
+
 rally deployment config > depl.json
 
 grep -v "\"endpoint_type\": \"internal\"," depl.json > temp && mv temp depl.json
@@ -17,13 +21,4 @@ rally deployment show
 git clone https://github.com/kotavi/mos-initial-check-rally-scenarios.git
 cd mos-initial-check-rally-scenarios/
 ./combine_files.py --filename all_scenarios.yaml
-rally task start all_scenarios.yaml --task-args-file task_arguments.yaml
-
-
-
-source /home/rally/openrc
-rally-manage db recreate
-rally deployment create --fromenv --name=tempest
-rally verify create-verifier --name tempest_tests --type tempest --source https://github.com/openstack/tempest.git
-rally verify list-verifiers
-rally verify show-verifier
+echo 'rally task start all_scenarios.yaml --task-args-file task_arguments.yaml'
